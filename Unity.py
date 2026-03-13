8ae74d083f5d1789b1cd21d9afedc4349bf2c7c5
meteor-shower-l <1637624038@qq.com>
2026-03-13 15:35:23 +0800

    添加寝室类，并进行实现了对话函数

--- Unity.py
+++ Unity.py
@@ -1,10 +1,8 @@
-
-
 import threading
 
+import itchat
 import numpy
 import requests
-import itchat
 from bs4 import BeautifulSoup
 
 
@@ -33,13 +31,60 @@ class Human:
         print("强敌我斩，坚甲我摧")
 
     def compare(self):
-        attribute = ['soFree', 'CH圈', 'OP', '米卫兵', '独立女性', '孙吧黄牌', '神神', '粉红', '黄毛', '精神小妹']
-        attribute_sort = sorted(attribute, key=lambda
-            x: 1 if x == '独立女性' else 2 if x == 'OP' else 3 if x == '米卫兵' else 4 if x == '孙吧黄牌' else 5 if x == '精神小妹' else 6 if x == '黄毛' else 7 if x == '神神' else 8 if x == 'soFree' else 9 if x == 'CH圈' else 10)
-        print(f'从拉到夯{attribute_sort}')
+        attribute = [
+            "soFree",
+            "CH圈",
+            "OP",
+            "米卫兵",
+            "独立女性",
+            "孙吧黄牌",
+            "神神",
+            "粉红",
+            "黄毛",
+            "精神小妹",
+        ]
+        attribute_sort = sorted(
+            attribute,
+            key=lambda x: (
+                1
+                if x == "独立女性"
+                else (
+                    2
+                    if x == "OP"
+                    else (
+                        3
+                        if x == "米卫兵"
+                        else (
+                            4
+                            if x == "孙吧黄牌"
+                            else (
+                                5
+                                if x == "精神小妹"
+                                else (
+                                    6
+                                    if x == "黄毛"
+                                    else (
+                                        7
+                                        if x == "神神"
+                                        else (
+                                            8
+                                            if x == "soFree"
+                                            else 9 if x == "CH圈" else 10
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            ),
+        )
+        print(f"从拉到夯{attribute_sort}")
+
     def YSQD(self):
-        if self.attribute=='OP':
-            print('米叠的恩情还不完')
+        if self.attribute == "OP":
+            print("米叠的恩情还不完")
+
     def shrimp_head_guy(self):
         response = requests.get("https://www.shift-journal.org/library")
         soup = BeautifulSoup(response.text, "html.parser")
@@ -55,13 +100,39 @@ class Human:
         for i in range(thread_num):
             t = threading.Thread(target=self.singel_attack_shift)
             t.start
+
+
 class YourGirlFriend(Human):
     def __init__(self):
         super().__init__()
 
-    def MeetYellowHair(self,name):
-        self.Owner=name
-        print(f'我是{name}大人的狗')
+    def MeetYellowHair(self, name):
+        self.Owner = name
+        print(f"我是{name}大人的狗")
+
+
+class d1_308:
+    def __init__(self):
+        self.people = []
+        self.activity = ""
+        self.sentences = [
+            "对面纯轮椅",
+            "累挺",
+            "有人去打水吗",
+            "哎呀！今天洛克王国还没打呢",
+            "晾衣叉在你柜子里",
+            "老蒋,打mc吗",
+            "我今天有点累，不是很想打，但我没说我不打",
+            "你阿诺吧",
+            "烫烫烫",
+            "锟斤拷",
+        ]
+
+    def talk(self):
+        for i in range(100):
+            for j in range(len(self.sentences)):
+                print(self.sentences[j])
+
 
 meteor_shower = Human()
 meteor_shower.attribute, meteor_shower.voice = "soFree", "萝莉音"
@@ -74,22 +145,24 @@ if hasattr(meteor_shower, "location") and meteor_shower.location == "library":
     guaranteed_postgraduate_admission_elixir = True
     print("赢麻了")
 
+
 def behave():
-    a,b,c,d='牢魏','延庆区长','刘金³习²','🐔仲谋'
+    a, b, c, d = "牢魏", "延庆区长", "刘金³习²", "🐔仲谋"
     if a and d:
-        print(f'{a}，有没有吃的')
-        print('我的🐔breast你吃不吃')
-    if b in ['D1-308'] and [a,c] not in['D1-308']:
-        print('🦌')
-        print('石传说')
+        print(f"{a}，有没有吃的")
+        print("我的🐔breast你吃不吃")
+    if b in ["D1-308"] and [a, c] not in ["D1-308"]:
+        print("🦌")
+        print("石传说")
     if c and d:
-        print(f'{d}')
-        print(f'{c}纯human&chicken')
-        
-if 'attack_shift' in dir(meteor_shower) and threading.active_count() > 2:
+        print(f"{d}")
+        print(f"{c}纯human&chicken")
+
+
+if "attack_shift" in dir(meteor_shower) and threading.active_count() > 2:
     itchat.auto_login()
     rooms = itchat.get_chatrooms()
     for room in rooms:
-        if '禁水群' in room['NickName']:
-            room.send('我爱gx')
+        if "禁水群" in room["NickName"]:
+            room.send("我爱gx")
             break
