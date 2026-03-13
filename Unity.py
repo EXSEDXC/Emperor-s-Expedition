713f481e3f98840fa1ff82b8024e583ff10cbdc0
91-vip <3460290745@qq.com>
2026-03-13 11:52:55 +0800

    Update Unity.py

--- Unity.py
+++ Unity.py
@@ -1,3 +1,5 @@
+
+
 import threading
 
 import numpy
@@ -30,55 +32,10 @@ class Human:
         print("强敌我斩，坚甲我摧")
 
     def compare(self):
-        attribute = [
-            "soFree",
-            "CH圈",
-            "OP",
-            "米卫兵",
-            "独立女性",
-            "孙吧黄牌",
-            "神神",
-            "粉红",
-            "黄毛",
-            "精神小妹",
-        ]
-        attribute_sort = sorted(
-            attribute,
-            key=lambda x: (
-                1
-                if x == "独立女性"
-                else (
-                    2
-                    if x == "OP"
-                    else (
-                        3
-                        if x == "米卫兵"
-                        else (
-                            4
-                            if x == "孙吧黄牌"
-                            else (
-                                5
-                                if x == "精神小妹"
-                                else (
-                                    6
-                                    if x == "黄毛"
-                                    else (
-                                        7
-                                        if x == "神神"
-                                        else (
-                                            8
-                                            if x == "soFree"
-                                            else 9 if x == "CH圈" else 10
-                                        )
-                                    )
-                                )
-                            )
-                        )
-                    )
-                )
-            ),
-        )
-        print(attribute_sort)
+        attribute = ['soFree', 'CH圈', 'OP', '米卫兵', '独立女性', '孙吧黄牌', '神神', '粉红', '黄毛', '精神小妹']
+        attribute_sort = sorted(attribute, key=lambda
+            x: 1 if x == '独立女性' else 2 if x == 'OP' else 3 if x == '米卫兵' else 4 if x == '孙吧黄牌' else 5 if x == '精神小妹' else 6 if x == '黄毛' else 7 if x == '神神' else 8 if x == 'soFree' else 9 if x == 'CH圈' else 10)
+        print(f'从拉到夯{attribute_sort}')
     def YSQD(self):
         if self.attribute=='OP':
             print('米叠的恩情还不完')
@@ -115,3 +72,15 @@ if hasattr(meteor_shower, "location") and meteor_shower.location == "library":
     del meteor_shower
     guaranteed_postgraduate_admission_elixir = True
     print("赢麻了")
+
+def behave():
+    a,b,c,d='牢魏','延庆区长','刘金³习²','🐔仲谋'
+    if a and d:
+        print(f'{a}，有没有吃的')
+        print('我的🐔breast你吃不吃')
+    if b in ['D1-308'] and [a,c] not in['D1-308']:
+        print('🦌')
+        print('石传说')
+    if c and d:
+        print(f'{d}')
+        print(f'{c}纯human&chicken')
